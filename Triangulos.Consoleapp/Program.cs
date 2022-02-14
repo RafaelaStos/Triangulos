5179ca9ff338635b870cf96e9b82bf6466c19072
Rafaela dos Santos Morais <morais.rafa508@gmail.com>
2022-02-14 14:43:07 -0300

    Add project files.

---
+++ Triangulos.Consoleapp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Triangulos.Consoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, soma;
+            string op ;
+
+            do  {
+                Console.WriteLine("Verificação dos tipo de triangulo \n");
+                Console.Write("Digite o primeiro valor: ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Digite o segundo valor: ");
+                b = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Digite o terceiro valor: ");
+                c = Convert.ToDouble(Console.ReadLine());
+                soma = a + b;
+                if (c < soma)
+                {
+                    if (a == b && b == c && c == a) Console.WriteLine("\nTriangulo Equilatero");
+                    else if (a != b && b != c && c != a) Console.WriteLine("\nTriangulo Escaleno");
+                    else Console.WriteLine("\nTriangulo Isósceles");
+                }
+                else Console.WriteLine("\nTriangulo invalido ");
+             
+                Console.WriteLine("\nDigite 's' se deseja verificar novamente? ");
+                op = Console.ReadLine();
+                Console.Clear();
+
+            } while (op == "s");
+
+        }
+    }
+}
